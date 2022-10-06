a4e8910a44be41f9da481d071a9a0e2a739817b7
Rishika Saraogi <saraogirishika31@gmail.com>
2022-10-06 11:15:01 +0530

    my first commit

--- public/js/main.js
+++ public/js/main.js
@@ -5,27 +5,30 @@ const cityName = document.getElementById('cityName');
 //get output here of form 
 const city_name = document.getElementById('city_name');
 //getting temp
-const temp = document.getElementById('temp');
+const temp = document.getElementById('temp_real_val');
 //getting temp_status
 const temp_status = document.getElementById('temp_status');
+//hide the temp infos when nothing is inputed
+const datahide = document.querySelector('.middle_layer');
 
 const getInfo = async(event)=>{
     event.preventDefault();
     let cityVal = cityName.value;
     if(cityVal===""){
         city_name.innerText = `Write city name before search`;
-        console.log("hello");
+        //console.log("hello");
+        datahide.classList.add('data_hide');
     }else{
         try{
             
-            let url = `api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=4f390e443b5c5383a849aa38d017add4`;
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=4f390e443b5c5383a849aa38d017add4`;
             const response = await fetch(url);
             
             //console.log(response);
             //converting data to json file. 
             console.log("hello");
             const data = await response.json();
-            alert(cityVal);
+            //alert(cityVal);
             //converting data to array 
             const arrData = [data];
             
@@ -34,9 +37,7 @@ const getInfo = async(event)=>{
             //look for api data to see how to get to data from api -ie how to fetch data using indexing 
             city_name.innerText = `${arrData[0].name} , ${arrData[0].sys.country}`;
             temp.innerText = arrData[0].main.temp;
-            temp_status.innerText = arrData[0].weather[0].main;
 
-            
             //code for icons
             const tempMood = arrData[0].weather[0].main;
             if (tempMood==="Clear"){
@@ -47,10 +48,14 @@ const getInfo = async(event)=>{
                 temp_status.innerHTML= "<i class='fas fa-cloud-rain' style = 'color:#a4b0be;'></i>";
             }
             else{
-                temp_status.innerHTML= "<i class='fas fa-cloud' style = 'color:#f1f2f6;'></i>";
+                temp_status.innerHTML= "<i class='fas fa-sun' style = 'color:#eccc68;'></i>";
             }
+
+            datahide.classList.remove('data_hide');
+
         }catch{
             city_name.innerText = `Enter proper city name`;
+            datahide.classList.add('data_hide');
         }   
     }
 }
